3a5a5125206d217ce602f9f0a1ecfe81d38534a0
jijnasu <guddu.jijnasu999@gmail.com>
2023-12-20 22:02:21 +0530

    my_model cache added

--- application.py
+++ application.py
@@ -2,7 +2,7 @@
 
 
 
-import gradio as gr
+# import gradio as gr
 import numpy as np
 import os
 
@@ -44,6 +44,14 @@ def load_and_preprocess_image(img_path, target_size=(260, 260)):
     # img_array /= 255.0  # Normalize pixel values to [0, 1]
     return img_array
 
+def classify_image(image, model):
+    image = image.reshape((-1, ims, ims, 3))
+    prediction = model.predict(image).flatten()
+    confidences = {labels[i]: float(prediction[i]) for i in range(num_classes)}
+    return confidences
+
+
+@st.cache_resource(max_entries=1, hash_funcs={"MyUnhashableClass": lambda _: None})
 def my_model():
     # data_augmentation = tf.keras.Sequential([
     #     layers.RandomCrop(height=224, width=224),      
@@ -69,6 +77,11 @@ def my_model():
     outputs = keras.layers.Dense(num_classes, activation='softmax')(x)
     model = keras.Model(inputs, outputs)
 
+    # running for the first time
+    img = image.load_img('image.jpg', target_size=(ims,ims))
+    img = image.img_to_array(img)
+    classify_image(img, model)
+
     return model
 
 ims = 260
@@ -80,16 +93,6 @@ model.load_weights(weight_dir)
 model.build(keras.layers.InputLayer(input_shape=(ims, ims, 3)))
 
 
-def classify_image(image):
-    image = image.reshape((-1, ims, ims, 3))
-    prediction = model.predict(image).flatten()
-    confidences = {labels[i]: float(prediction[i]) for i in range(num_classes)}
-    return confidences
-
-img = image.load_img('image.jpg', target_size=(ims,ims))
-img = image.img_to_array(img)
-
-classify_image(img)
 
 
 if __name__=="__main__":
@@ -113,7 +116,7 @@ if __name__=="__main__":
         img_array = load_and_preprocess_image(uploaded_file)
 
         # Classify the image
-        result = classify_image(img_array)
+        result = classify_image(img_array, model)
         st.write("Classification Result:")
 
         # Display individual status bars for each class
@@ -126,3 +129,207 @@ if __name__=="__main__":
     st.subheader('About:')
     st.write('by Kumar Jijnasu')
     st.link_button('github.com/jijnasu','https://github.com/jijnasu')
+
+
+
+
+# ==================================================================
+
+    # st.divider()
+    
+    # test_img_dir = "Testing/glioma_tumor/"
+    # sample_images = ["image.jpg", "image(1).jpg", "image(2).jpg", "image(3).jpg"]
+    # sample_images = [os.path.join(test_img_dir, image) for image in sample_images]
+# ==================================================================
+    # with st.container():
+    #     # components
+    #     components.html(
+    #         """
+
+
+
+
+
+
+
+    #             <style>
+    #                 body {
+    #                     overflow-x: hidden; /* Hide horizontal scrollbar on the body */
+    #                 }
+
+    #                 .image-container {
+    #                     display: flex;
+    #                     overflow-x: auto; /* Enable horizontal scrollbar for the container */
+    #                     white-space: nowrap; /* Prevent images from wrapping to the next line */
+    #                 }
+
+    #                 .image-container img {
+    #                     width: 300px; /* Adjust the width of the images as needed */
+    #                     height: auto;
+    #                     margin-right: 10px; /* Adjust the margin between images as needed */
+    #                 }
+    #             </style>
+
+
+
+    #             <div>
+    #                 <img src="image.jpg" alt="Image 1">
+    #             </div>
+
+
+
+    #         """,
+    #     )
+
+    
+    # # with st.container():
+    # #     st.markdown('<div class="image-container">', unsafe_allow_html=True)
+    # #     for image_path in image_paths:
+    # #         st.image(image_path, width=100, caption=os.path.basename(image_path), use_column_width=False, output_format="auto")
+    # #     st.markdown('</div>', unsafe_allow_html=True)
+    # # with st.container():
+    #     # st.components.v1.streamlit.image
+    #     components.html('<div class="image-container">')
+    #     for image_path in sample_images:
+    #         st.image(image_path, width=100, caption=os.path.basename(image_path), use_column_width=False, output_format="auto")
+    #     components.html('</div>')
+
+
+
+    # # Custom HTML and CSS for horizontally scrolling image gallery
+    # image_gallery_html = f"""
+    #     <div class="image-container">
+    #         {" ".join([f'<img src="{image}" alt="Image"></img>' for image in sample_images])}
+    #     </div>
+    # """
+
+    # # Bootstrap 4 collapse example with added image gallery
+    # components.html(
+    #     f"""
+    #     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    #     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    #     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        
+    #     <div id="accordion">
+    #     <div class="card">
+    #         <div class="card-header" id="headingOne">
+    #         <h5 class="mb-0">
+    #             <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+    #             Collapsible Group Item #1
+    #             </button>
+    #         </h5>
+    #         </div>
+    #         <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+    #         <div class="card-body">
+    #             {image_gallery_html}
+    #         </div>
+    #         </div>
+    #     </div>
+    #     <div class="card">
+    #         <div class="card-header" id="headingTwo">
+    #         <h5 class="mb-0">
+    #             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+    #             Collapsible Group Item #2
+    #             </button>
+    #         </h5>
+    #         </div>
+    #         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+    #         <div class="card-body">
+    #             {image_gallery_html}
+    #         </div>
+    #         </div>
+    #     </div>
+    #     </div>
+    #     """
+    # )
+
+    # def horizontal_image_gallery(image_paths):
+    #     # Display images horizontally using st.image
+    #     st.markdown(
+    #         """
+    #         <style>
+    #             .collapsible {
+    #                 cursor: pointer;
+    #                 padding: 18px;
+    #                 width: 100%;
+    #                 border: none;
+    #                 text-align: left;
+    #                 outline: none;
+    #                 font-size: 15px;
+    #             }
+
+    #             .content {
+    #                 display: none;
+    #                 overflow: hidden;
+    #             }
+
+    #             .image-container {
+    #                 display: flex;
+    #                 overflow-x: auto;
+    #                 white-space: nowrap;
+    #             }
+
+    #             .image-container img {
+    #                 width: 300px;
+    #                 height: auto;
+    #                 margin-right: 10px;
+    #             }
+    #         </style>
+    #         """,
+    #         unsafe_allow_html=True
+    #     )
+
+    #     for i, image_path in enumerate(image_paths):
+    #         st.markdown(
+    #             f"""
+    #             <button class="collapsible" onclick="toggleContent({i})">Collapsible Group Item #{i + 1}</button>
+    #             <div class="content" id="content{i}">
+    #                 <div class="image-container">
+    #                     <img src="{image_path}" alt="Image">
+    #                 </div>
+    #             </div>
+    #             """,
+    #             unsafe_allow_html=True
+    #         )
+    # horizontal_image_gallery(sample_images)
+# ========================================================================
+    # Function to create the custom HTML with embedded CSS and JavaScript
+    # def horizontal_image_gallery(image_paths):
+    #     # Display images horizontally using st.image
+    #     st.markdown(
+    #         """
+    #         <style>
+    #             .image-container {
+    #                 display: flex;
+    #                 overflow-x: auto;
+    #                 white-space: nowrap;
+    #             }
+
+    #             .image-container img {
+    #                 width: 300px;
+    #                 height: auto;
+    #                 margin-right: 10px;
+    #             }
+    #         </style>
+    #         Example
+    #         """,
+    #         unsafe_allow_html=True
+    #     )
+
+    #     with st.container():
+    #         st.markdown('<div class="image-container">', unsafe_allow_html=True)
+    #         for image_path in image_paths:
+    #             st.image(image_path, width=100, caption=os.path.basename(image_path), use_column_width=False, output_format="auto")
+    #         st.markdown('</div>', unsafe_allow_html=True)
+
+    # # Sample image paths
+    # test_img_dir = "Testing/glioma_tumor/"
+    # sample_images = ["image.jpg", "image(1).jpg", "image(2).jpg", "image(3).jpg"]
+
+    # # Construct absolute paths
+    # sample_images = [os.path.join(test_img_dir, image) for image in sample_images]
+
+    # # Display the horizontally scrollable image gallery
+    # horizontal_image_gallery(sample_images)
+
+    # components.html()
